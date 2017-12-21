8a748736065c629d499bc0db9c9e7767b3c1b233
Bluewind12 <zero18030@gmail.com>
2017-12-21 18:57:36 +0900

    test

---
+++ Main.java
@@ -0,0 +1,26 @@
+//Create By Momose
+/**
+ * Main
+ */
+public class Main {
+    int Plus(int alpha, int beta) {
+        System.out.println(alpha + " + " + beta + " = " + (alpha + beta));
+        return alpha + beta;
+    }
+
+    int Minus(int alpha, int beta) {
+        System.out.println(alpha + " - " + beta + " = " + (alpha - beta));
+        return alpha - beta;
+    }
+
+    int Multiply(int alpha, int beta) {
+        System.out.println(alpha + " * " + beta + " = " + (alpha * beta));
+        return alpha * beta;
+    }
+
+    int Division(int alpha, int beta) {
+        System.out.println(alpha + " / " + beta + " = " + (alpha / beta));
+        return alpha / beta;
+    }
+
+}
