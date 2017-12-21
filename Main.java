94f467aa252c4056db122f1739c859d9c58e483b
Bluewind12 <zero18030@gmail.com>
2017-12-21 18:58:42 +0900

    説明記述()

--- Main.java
+++ Main.java
@@ -3,21 +3,25 @@
  * Main
  */
 public class Main {
+    //‘«‚µŽZ
     int Plus(int alpha, int beta) {
         System.out.println(alpha + " + " + beta + " = " + (alpha + beta));
         return alpha + beta;
     }
 
+    //ˆø‚«ŽZ
     int Minus(int alpha, int beta) {
         System.out.println(alpha + " - " + beta + " = " + (alpha - beta));
         return alpha - beta;
     }
 
+    //Š|‚¯ŽZ
     int Multiply(int alpha, int beta) {
         System.out.println(alpha + " * " + beta + " = " + (alpha * beta));
         return alpha * beta;
     }
 
+    //Š„‚èŽZ
     int Division(int alpha, int beta) {
         System.out.println(alpha + " / " + beta + " = " + (alpha / beta));
         return alpha / beta;
